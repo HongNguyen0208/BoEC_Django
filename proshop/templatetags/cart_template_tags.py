93230d7db126279e9738016aa76077ae1e5c4b01
HongNguyen0208 <hongn5234@gmail.com>
2021-06-16 11:10:46 +0700

    product detail

--- proshop/templatetags/cart_template_tags.py
+++ proshop/templatetags/cart_template_tags.py
@@ -4,10 +4,12 @@ from proshop.models import Order
 register = template.Library()
 
 
-@register.filter
+@register.filter(name="cart_item_count")
 def cart_item_count(user):
     if user.is_authenticated:
         qs = Order.objects.filter(user=user, ordered=False)
         if qs.exists():
             return qs[0].items.count()
     return 0
+
+register.filter('cart_item_count', cart_item_count)
